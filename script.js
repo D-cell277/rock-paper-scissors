2db9f4aec4f039531276bf0de401f5ca7cc1371c
D-cell277 <gmtest445@gmail.com>
2022-08-22 20:41:37 +0530

    Add function for player choice.

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 const choices = ["rock", "paper", "scissors"]
-function getComputerChoice(choices) {
+function getComputerChoice() {
     var index =  Math.floor(Math.random() * choices.length);
-    return choices[index]
+    var computer = choices[index];
+    return computer;
+}
+function getPlayerChoice() {
+    var player = prompt("Type in your choice. Rock/Paper/Scissors: ").toLowerCase();
+    return player;
 }
 
+const computerChoice = getComputerChoice();
+const playerChoice = getPlayerChoice();
+
+function state() {
+    return console.log("Computer: " + computerChoice+ "\nPlayer: " + playerChoice)
+}
+state()
+
+
+function playRound(computerChoice,playerChoice) {
+    if (computerChoice == playerChoice) {
+        state()
+        console.log("It's a Draw.")
+    }
+playRound()
